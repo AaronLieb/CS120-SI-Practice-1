4debe567c809c96e99643b7c90af40b8174553de
AaronLieb <aarnliebmrn@gmail.com>
2020-09-16 15:32:17 -0700

    added dataTypes.cpp and math.cpp

---
+++ dataTypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int integer = 5;
+	double decimal = 2.5;
+	char character = 'C';
+	string word = "Carrot";
+	bool boolean = true;
+
+	cout << "This is a demonstration of several of the different data types in C++\n\n";
+	cout << "Integer (int):\nStores whole numbers.\nDoes not store decimals.\nSize: " << sizeof(integer) << " byte(s)\n\n";
+	cout << "Double (double):\nA floating point decimal number.\nSize: " << sizeof(decimal) << " byte(s)\n\n";
+	cout << "Character (char):\nA single character.\nWritten in code using single quotes (' ').\nStored in memory as an ascii number in the range 0-127.\nSize: " << sizeof(character) << " byte(s)\n\n";
+	cout << "String (string):\nA string or array of characters.\nSize determined by length of string.\nWritten in code using double quotes (\" \")\n\n";
+	cout << "Boolean (bool):\nA true or false value stored in memory as 0 or 1.\nSize: " << sizeof(boolean) << " byte(s).\nActual size of booleans is 1 bit.\nMultiple booleans can be stored in 1 byte.\n";
+}
---
+++ math.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "This program will be demonstrating the different math operations found in c++\n";
+	cout << "4 + 3 = " << 3 + 4 << "\n";
+	cout << "4 - 3 = " << 3 - 4 << "\n";
+	cout << "4 * 3 = " << 3 * 4 << "\n";
+	cout << "(integer) 4 / 3 = " << 3/4 << "\n";
+	cout << "(double) 4.0 / 3.0 = " << 3.0/4.0 << "\n";
+	cout << "4 % 3 = " << 4 % 3 << "\n";
+	cout << "3 % 4 = " << 3 % 4 << "\n";
+
+}
