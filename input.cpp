a0216bdb49e4e95a407ad08b8351e9483bf707ae
AaronLieb <aarnliebmrn@gmail.com>
2020-09-23 15:19:18 -0700

    added input.cpp

---
+++ input.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Welcome to the console input practice!\n";
+
+	/* Lets start by getting the users name
+	 * 1. Create a variable to store the name
+	 * 2. Output a message asking for their name
+	 * 3. Use cin to extract the user input
+	 * 4. Print a welcome message using their name */
+
+	/* Start code here */
+
+
+
+	/* End code here */
+
+
+
+	/* Now lets ask the user for their age
+	 * Follow the previous steps to extract the input
+	 * Print a message confirming their age */
+
+	/* Start code here */
+
+
+
+	/* End code here */
+
+
+
+	/* Finally, ask the user for their city and state
+	 * Example: Fullerton California
+	 * Create two variables for city and state
+	 * Use cin to extract both inputs in one line
+	 * Print a message displaying all of the information collected thus far */
+
+	/* Start code here */
+
+
+
+	/* End code here */
+
+	return 0;
+}
