4f6be04465897a8adfb90640b3789afd17dce39c
AaronLieb <aarnliebrmn@gmail.com>
2020-10-07 16:55:48 -0700

    updatd

--- midterm/random.cpp
+++ midterm/random.cpp
@@ -8,5 +8,8 @@ int main() {
 	srand(time(0));
 	cout << "rand(): " << rand() << "\n";
 	cout << "rand() % 10: " << rand() % 10 << "\n";
-	cout << "rand() % 10 + 1: " << rand() % 10 + 1 << "\n";
+	cout << "rand() % 10 + 1: " << rand() % (201) + 300 << "\n";
 }
+
+// Random numbers in the range [min, max]
+// rand() % (max-min+1) + min
