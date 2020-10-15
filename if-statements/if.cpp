0d86f70a92cc55ca0d340dda515dcce239a60082
AaronLieb <aarnliebrmn@gmail.com>
2020-10-14 17:07:20 -0700

    finished code

--- if-statements/if.cpp
+++ if-statements/if.cpp
@@ -8,7 +8,17 @@ int main() {
 	cout << "How old are you? ";
 	cin >> age;
 
-	if (/*???*/) {
-		cout << "You are an adult!\n";
+	if (age >= 18) {
+		if (age >= 55) {
+			cout << "You are old!\n";
+		} else {
+			cout << "You are an adult!\n";
+		}
+	} else if (age == 0) {
+		cout << "You are in the womb\n";
+	} else if (age < 18) {
+		cout << "You are a minor.\n";
+	} else {
+		cout << "You are not born yet.\n";
 	}
 }
